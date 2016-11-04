df63ae8274be47120cd7aae61d0216cbecfeb8d8
Talon Forbush <Talon Forbush>
2016-11-04 07:30:22 -0600

    GapShield Start Moved existing test scripts into a folder Created script to create dev DB

---
+++ Scripts/Create-Ecoological-Dev.ps1
@@ -0,0 +1,11 @@
+﻿$credential = Get-Credential
+$uname = $credential.UserName
+$pass =  $credential.GetNetworkCredential().Password
+$server = "ecooldb.ccd1ufibwvil.us-west-1.rds.amazonaws.com"
+$db = "Ecooldev"
+
+$command = "USE master;
+IF EXISTS(SELECT * FROM sys.databases WHERE name = '$db') DROP DATABASE $db;
+CREATE DATABASE testdb;"
+
+Invoke-Sqlcmd -ServerInstance $server -Username $uname -Password $pass -Query $command
