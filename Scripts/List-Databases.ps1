c8d2fd757fa8ad52326070625c3fde7fe755e8d4
Talon Forbush <Talon Forbush>
2016-11-04 00:26:26 -0600

    List-databases Upated Added a line to list all tables in a specified DB

--- Scripts/List-Databases.ps1
+++ Scripts/List-Databases.ps1
@@ -8,3 +8,8 @@ $command = "USE master;
     SELECT name, database_id, create_date FROM sys.databases;"
 
 Invoke-Sqlcmd -ServerInstance $server -Username $uname -Password $pass -Query $command
+
+#for listing tables
+
+$command2 = "SELECT TABLE_NAME FROM $db.INFORMATION_SCHEMA.Tables WHERE TABLE_TYPE = 'BASE TABLE'"
+Invoke-Sqlcmd -ServerInstance $server -Username $uname -Password $pass -Query $command2
