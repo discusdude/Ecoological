4cc7a208547872c0e31d33741902570edb4b2e8b
Talon Forbush <Talon Forbush>
2016-11-04 00:24:32 -0600

    Testing Finished a simple constraints script for auto incrementing value and primary key.

---
+++ Scripts/Constraints.ps1
@@ -0,0 +1,19 @@
+﻿$credential = Get-Credential
+$uname = $credential.UserName
+$pass =  $credential.GetNetworkCredential().Password
+$server = "ecooldb.ccd1ufibwvil.us-west-1.rds.amazonaws.com"
+$db = "testdb"
+
+
+$command = "
+    IF OBJECT_ID('individual','U') IS NOT NULL
+        DROP TABLE individual;
+    CREATE TABLE individual(
+        ID int IDENTITY,
+        lastname varchar(255),
+        firstname varchar(255),
+        PRIMARY KEY(ID)
+    );
+"
+
+Invoke-Sqlcmd -ServerInstance $server -Database $db -Username $uname -Password $pass -Query $command
