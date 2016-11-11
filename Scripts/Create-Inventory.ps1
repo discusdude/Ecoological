b54b45e5710a436a92c0d0fb53ff267f263ce986
Talon Forbush <Talon Forbush>
2016-11-10 21:23:59 -0700

    inventory v2 added content to inventory creation file. It is simple and doesn't have any constraints yet.

--- Scripts/Create-Inventory.ps1
+++ Scripts/Create-Inventory.ps1
@@ -5,7 +5,15 @@ $server = "ecooldb.ccd1ufibwvil.us-west-1.rds.amazonaws.com"
 $db = "Ecooldev"
 
 $command = "
-    CREATE TABLE inventory(
-        
-    )
+    IF OBJECT_ID('inventories','U') IS NOT NULL
+        DROP TABLE inventories;
+    CREATE TABLE inventories(
+        invid INT IDENTITY,
+        date DATE,
+        type VARCHAR(255),
+        count INT,
+        PRIMARY KEY(invid)
+    );
 "
+
+Invoke-Sqlcmd -ServerInstance $server -Database $db -Username $uname -Password $pass -Query $command
