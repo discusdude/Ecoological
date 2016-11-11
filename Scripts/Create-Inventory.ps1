79f6e9188afd413426f52db432fb0a8b86144665
Talon Forbush <Talon Forbush>
2016-11-10 20:14:46 -0700

    started inventory

---
+++ Scripts/Create-Inventory.ps1
@@ -0,0 +1,11 @@
+﻿$credential = Get-Credential
+$uname = $credential.UserName
+$pass =  $credential.GetNetworkCredential().Password
+$server = "ecooldb.ccd1ufibwvil.us-west-1.rds.amazonaws.com"
+$db = "Ecooldev"
+
+$command = "
+    CREATE TABLE inventory(
+        
+    )
+"
