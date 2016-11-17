1f50463fb2815e91518e75564ad3964b8f4f8348
Talon Forbush <discusdude14@gmail.com>
2016-11-17 00:06:47 -0700

    New Supplies Changed the supplies table structure. Added basePrices, discounts, conditions, and supplynames tables.

--- Scripts/Create-Products.ps1
+++ Scripts/Create-Products.ps1
@@ -21,57 +21,57 @@ $command = "
         DROP TABLE AreoBox;
 
 
-    CREATE TABLE ProductTypes(
-        ProductTypeID int,
-        ProductType varchar(20),
-        PRIMARY KEY(ProductTypeID)
-    );
+    --CREATE TABLE ProductTypes(
+    --    ProductTypeID int,
+    --    ProductType varchar(20),
+    --    PRIMARY KEY(ProductTypeID)
+    --);
 
-    INSERT INTO ProductTypes(
-        ProductTypeID,
-        ProductType
-    )
-    VALUES
-    (1,'Bumper Shellz'),
-    (2,'Gap Sheild'),
-    (3,'Areo Box');
+    --INSERT INTO ProductTypes(
+    --    ProductTypeID,
+    --    ProductType
+    --)
+    --VALUES
+    --(1,'Bumper Shellz'),
+    --(2,'Gap Sheild'),
+    --(3,'Areo Box');
 
-    CREATE TABLE products(
-        ProdID INT IDENTITY,
-        ProductTypeID int REFERENCES ProductTypes(ProductTypeID) NOT NULL,
-        price MONEY,
-        location INT,
-        PRIMARY KEY(ProdID),
-        CONSTRAINT Product_AltPK unique (ProdID, ProductTypeID)
-    );
+    --CREATE TABLE products(
+    --    ProdID INT IDENTITY,
+    --    ProductTypeID int REFERENCES ProductTypes(ProductTypeID) NOT NULL,
+    --    price MONEY,
+    --    location INT,
+    --    PRIMARY KEY(ProdID),
+    --    CONSTRAINT Product_AltPK unique (ProdID, ProductTypeID)
+    --);
 
-    CREATE TABLE BumperShellz(
-        ProdID INT,
-        ProductTypeID as 1 persisted,
-        Condition varchar(20),
-        Status varchar(20),
-        SKU int,
-        Count int,
-        PRIMARY KEY(ProdID),
-        FOREIGN KEY(ProdID, ProductTypeID) REFERENCES products(ProdID, ProductTypeID)
-    )
+    --CREATE TABLE BumperShellz(
+    --   ProdID INT,
+    --    ProductTypeID as 1 persisted,
+    --    Condition varchar(20),
+    --    Status varchar(20),
+    --    SKU int,
+    --    Count int,
+    --    PRIMARY KEY(ProdID),
+    --    FOREIGN KEY(ProdID, ProductTypeID) REFERENCES products(ProdID, ProductTypeID)
+    --)
 
-    CREATE TABLE GapShields(
-        ProdID INT,
-        ProductTypeID as 2 persisted,
-        Count int,
-        PRIMARY KEY(ProdID),
-        FOREIGN KEY(ProdID, ProductTypeID) REFERENCES products(ProdID, ProductTypeID)
-    )
+    --CREATE TABLE GapShields(
+    --    ProdID INT,
+    --    ProductTypeID as 2 persisted,
+    --    Count int,
+    --    PRIMARY KEY(ProdID),
+    --    FOREIGN KEY(ProdID, ProductTypeID) REFERENCES products(ProdID, ProductTypeID)
+    --)
 
-    CREATE TABLE AreoBox(
-        ProdID INT,
-        ProductTypeID as 3 persisted,
-        SKU int,
-        Count int,
-        PRIMARY KEY(ProdID),
-        FOREIGN KEY(ProdID, ProductTypeID) REFERENCES products(ProdID, ProductTypeID)
-    )
+    --CREATE TABLE AreoBox(
+    --    ProdID INT,
+    --    ProductTypeID as 3 persisted,
+    --    SKU int,
+    --    Count int,
+    --    PRIMARY KEY(ProdID),
+    --    FOREIGN KEY(ProdID, ProductTypeID) REFERENCES products(ProdID, ProductTypeID)
+    --)
 "
 
 Invoke-Sqlcmd -ServerInstance $server -Database $db -Username $uname -Password $pass -Query $command
