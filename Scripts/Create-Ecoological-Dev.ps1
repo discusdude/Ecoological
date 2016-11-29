5fd9b20e54631e4616e1cc53adbec6da0e811856
Talon Forbush <discusdude14@gmail.com>
2016-11-29 00:09:31 -0700

    Consolidation Consolidated install scripts into one sql file made minor error fixes added productName table a few other changes

--- Scripts/Create-Ecoological-Dev.ps1
+++ Scripts/Create-Ecoological-Dev.ps1
@@ -3,9 +3,14 @@ $uname = $credential.UserName
 $pass =  $credential.GetNetworkCredential().Password
 $server = "ecooldb.ccd1ufibwvil.us-west-1.rds.amazonaws.com"
 $db = "Ecooldev"
+$tableSQLPath = "$PSScriptRoot\sqltest.sql"
 
+#create database
 $command = "USE master;
 IF EXISTS(SELECT * FROM sys.databases WHERE name = '$db') DROP DATABASE $db;
 CREATE DATABASE $db;"
 
 Invoke-Sqlcmd -ServerInstance $server -Username $uname -Password $pass -Query $command
+
+#create tables
+Invoke-Sqlcmd -ServerInstance $server -Database $db -Username $uname -Password $pass -InputFile $tableSQLPath
--- Scripts/sqltest.sql
+++ Scripts/sqltest.sql
@@ -1,3 +1,102 @@
+--drop these tables if exist because they reference productNames and supplyNames
+IF OBJECT_ID('inventories','U') IS NOT NULL
+    DROP TABLE inventories;
+IF OBJECT_ID('sales','U') IS NOT NULL
+    DROP TABLE sales;
+
+--supplies
+IF OBJECT_ID('supplies','U') IS NOT NULL
+    DROP TABLE supplies;
+IF OBJECT_ID('basePrices','U') IS NOT NULL
+    DROP TABLE basePrices;
+IF OBJECT_ID('discounts','U') IS NOT NULL
+    DROP TABLE discounts;
+IF OBJECT_ID('supplyNames','U') IS NOT NULL
+    DROP TABLE supplyNames;
+IF OBJECT_ID('productNames','U') IS NOT NULL
+    DROP TABLE productNames;
+IF OBJECT_ID('conditions','U') IS NOT NULL
+    DROP TABLE conditions;
+
+--supplyNames for constraint
+CREATE TABLE supplyNames(
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(name)
+);
+--productNames for constraint
+CREATE TABLE productNames(
+    product VARCHAR(255) NOT NULL,
+    PRIMARY KEY(product)
+);
+
+--conditions for constraint
+CREATE TABLE conditions(
+    condition VARCHAR(255) NOT NULL,
+    PRIMARY KEY(condition)
+);
+
+CREATE TABLE discounts(
+    name VARCHAR(255) REFERENCES supplyNames(name) NOT NULL,
+    condition VARCHAR(255) REFERENCES conditions(condition) NOT NULL,
+    discount FLOAT NOT NULL,
+    PRIMARY KEY(name, condition),
+    CONSTRAINT chk_discount CHECK(discount BETWEEN 0 AND 1)
+);
+
+CREATE TABLE supplies(
+    name varchar(255) REFERENCES supplyNames(name) NOT NULL,
+    product VARCHAR(255) REFERENCES productNames(product) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    condition VARCHAR(255) REFERENCES conditions(condition) NOT NULL,
+    count INT,
+    PRIMARY KEY(name)
+);
+
+CREATE TABLE basePrices(
+    name VARCHAR(255) REFERENCES supplyNames(name) NOT NULL,
+    price MONEY NOT NULL,
+    PRIMARY KEY(name)
+);
+
+
+INSERT INTO supplyNames(name) VALUES
+('Display Boxes'),
+('Internet Boxes'),
+('Display Product'),
+('Internet Product'),
+('75 ft rolls'),
+('Aero Boxes'),
+('Bumper Shellz');
+
+INSERT INTO conditions(condition) VALUES
+('Good'),
+('Bad');
+
+INSERT INTO discounts(name, condition, discount) VALUES
+('Display Boxes', 'Good', 1),
+('Display Boxes', 'Bad', 0.5),
+('Internet Boxes', 'Good', 1),
+('Internet Boxes', 'Bad', 0.5),
+('Display Product', 'Good', 1),
+('Display Product', 'Bad', 0.5),
+('Internet Product', 'Good', 1),
+('Internet Product', 'Bad', 0.5),
+('75 ft rolls', 'Good', 1),
+('75 ft rolls', 'Bad', 0.5),
+('Aero Boxes', 'Good', 1),
+('Aero Boxes', 'Bad', 0.5),
+('Bumper Shellz', 'Good', 1),
+('Bumper Shellz', 'Bad', 0.5);
+
+INSERT INTO basePrices(name, price) VALUES
+('Display Boxes', 15.00),
+('Internet Boxes', 20.99),
+('Display Product', 9.99),
+('Internet Product', 7.00),
+('75 ft rolls', 5.00),
+('Aero Boxes', 200.00),
+('Bumper Shellz', 14.20);
+
 --Iventory
 IF OBJECT_ID('inventories','U') IS NOT NULL
     DROP TABLE inventories;
