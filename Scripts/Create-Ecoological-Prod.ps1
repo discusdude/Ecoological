8ba97ac1b30d38d591b77bfdb2b98932f1177599
mevans27 <mitchell.evans27@gmail.com>
2016-12-19 15:23:01 -0700

    Created Prod Script
    Made a Production Script wheere I changed the target database to
    EcoolProd. Wondering if I should make seperate table SQL scripts for
    prod as well.

---
+++ Scripts/Create-Ecoological-Prod.ps1
@@ -0,0 +1,16 @@
+﻿$credential = Get-Credential
+$uname = $credential.UserName
+$pass =  $credential.GetNetworkCredential().Password
+$server = "ecooldb.ccd1ufibwvil.us-west-1.rds.amazonaws.com"
+$db = "Ecoolprod"
+$tableSQLPath = "$PSScriptRoot\sqltest.sql"
+
+#create database
+$command = "USE master;
+IF EXISTS(SELECT * FROM sys.databases WHERE name = '$db') DROP DATABASE $db;
+CREATE DATABASE $db;"
+
+Invoke-Sqlcmd -ServerInstance $server -Username $uname -Password $pass -Query $command
+
+#create tables
+Invoke-Sqlcmd -ServerInstance $server -Database $db -Username $uname -Password $pass -InputFile $tableSQLPath
