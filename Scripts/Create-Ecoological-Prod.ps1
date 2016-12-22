3e34a1c88c75f62e7f7f939e8817e554b984cf20
mevans27 <mitchell.evans27@gmail.com>
2016-12-22 14:56:42 -0700

    Prod Script actually changes prod now!

--- Scripts/Create-Ecoological-Prod.ps1
+++ Scripts/Create-Ecoological-Prod.ps1
@@ -2,7 +2,7 @@
 $uname = $credential.UserName
 $pass =  $credential.GetNetworkCredential().Password
 $server = "ecooldb.ccd1ufibwvil.us-west-1.rds.amazonaws.com"
-$db = "Ecooldev"
+$db = "Ecoolprod"
 $tableSQLPath = "$PSScriptRoot\sqlprod.sql"
 
 #create database
