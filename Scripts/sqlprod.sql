db4071510f42760a61c376a21e80deb83fedc410
mevans27 <mitchell.evans27@gmail.com>
2016-12-27 19:16:04 -0700

    Structural changes
    Got more feetback from Grant, we need "rightLeftCenter" as a new column
    and primary key. I also changed a few of the varchars to 10 because they
    wont need any more and there is a limit to how many characters in a
    primary key.

--- Scripts/sqlprod.sql
+++ Scripts/sqlprod.sql
@@ -92,16 +92,17 @@ CREATE TABLE bumperShellz(
 	partNumber VARCHAR(255) NOT NULL,
 	truckModel VARCHAR(255) NOT NULL,
 	truckYear VARCHAR(255) NOT NULL,
-	sensorHoles VARCHAR(255) NOT NULL,
-	frontOrRear VARCHAR(255) NOT NULL,
-	fogLamps VARCHAR(255) NOT NULL,
-	exhaust VARCHAR(255) NOT NULL,
+	sensorHoles VARCHAR(10) NOT NULL,
+	frontOrRear VARCHAR(10) NOT NULL,	
+	rightLeftCenter VARCHAR(10) NOT NULL,
+	fogLamps VARCHAR(10) NOT NULL,
+	exhaust VARCHAR(10) NOT NULL,
 	UPC VARCHAR(255),
 	name varchar(255) NOT NULL,
-    condition VARCHAR(255) NOT NULL,
+    condition VARCHAR(10) NOT NULL,
     count INT,
 	price MONEY, 
-	PRIMARY KEY (partNumber,truckModel)
+	PRIMARY KEY (partNumber,truckModel,rightLeftCenter,condition)
 );
 
 CREATE TABLE basePrices(
