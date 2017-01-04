f4570ce9817a5643985cdcd6605c5095b32aee0d
mevans27 <mitchell.evans27@gmail.com>
2017-01-03 20:11:05 -0700

    Adding B-Stock Tables

--- Scripts/sqlprodedit.sql
+++ Scripts/sqlprodedit.sql
@@ -1,12 +1,53 @@
 --Iventory------------------------------------------------------------------------------------
-IF OBJECT_ID('plasticSheet','U') IS NOT NULL
-    DROP TABLE plasticSheet;
--- "plasticSheet" created by Mitchell Evans
-CREATE TABLE plasticSheet(
+ -- B-Stock BumperShellz
+ CREATE TABLE bumperShellzRight2B(
 	partNumber VARCHAR(255) NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	sheetLength VARCHAR(255) NOT NULL,
-	sheetWidth VARCHAR(255) NOT NULL,
-	count INT,
-	PRIMARY KEY (partNumber)
+	truckModel VARCHAR(255) NOT NULL,
+	truckYear VARCHAR(255) NOT NULL,
+	sensorHoles VARCHAR(10) NOT NULL,
+	frontOrRear VARCHAR(10) NOT NULL,	
+	fogLamps VARCHAR(10) NOT NULL,
+	exhaust VARCHAR(10) NOT NULL,
+	UPC VARCHAR(255),
+	name varchar(255) NOT NULL,
+    condition VARCHAR(10) NOT NULL,
+    count INT,
+	price MONEY, 
+	PRIMARY KEY (partNumber,truckModel,condition)
 );
+
+-- "bumperShellz" created by Mitchell Evans
+CREATE TABLE bumperShellzLeft2B(
+	partNumber VARCHAR(255) NOT NULL,
+	truckModel VARCHAR(255) NOT NULL,
+	truckYear VARCHAR(255) NOT NULL,
+	sensorHoles VARCHAR(10) NOT NULL,
+	frontOrRear VARCHAR(10) NOT NULL,	
+	fogLamps VARCHAR(10) NOT NULL,
+	exhaust VARCHAR(10) NOT NULL,
+	UPC VARCHAR(255),
+	name varchar(255) NOT NULL,
+    condition VARCHAR(10) NOT NULL,
+    count INT,
+	price MONEY, 
+	PRIMARY KEY (partNumber,truckModel,condition)
+);
+
+-- "bumperShellz" created by Mitchell Evans
+CREATE TABLE bumperShellzCenter2B(
+	partNumber VARCHAR(255) NOT NULL,
+	truckModel VARCHAR(255) NOT NULL,
+	truckYear VARCHAR(255) NOT NULL,
+	sensorHoles VARCHAR(10) NOT NULL,
+	frontOrRear VARCHAR(10) NOT NULL,	
+	fogLamps VARCHAR(10) NOT NULL,
+	exhaust VARCHAR(10) NOT NULL,
+	UPC VARCHAR(255),
+	name varchar(255) NOT NULL,
+    condition VARCHAR(10) NOT NULL,
+    count INT,
+	price MONEY, 
+	PRIMARY KEY (partNumber,truckModel,condition)
+);
+
+-- B-Stock BumperShellz End
