703945d8baa3d717ad672997d71d598840970618
mevans27 <mitchell.evans27@gmail.com>
2017-03-01 23:30:07 -0700

    Changing Cardboard Boxes Table

--- Scripts/sqlprodedit.sql
+++ Scripts/sqlprodedit.sql
@@ -1,38 +1,12 @@
 --Iventory------------------------------------------------------------------------------------	
-IF OBJECT_ID('bumperShellzRightLeft1','U') IS NOT NULL
-	DROP TABLE bumperShellzRightLeft1;
+IF OBJECT_ID('cardboardBoxes','U') IS NOT NULL
+	DROP TABLE cardboardBoxes;
  -- B-Stock BumperShellz
- CREATE TABLE bumperShellzRight1(
-	partNumber VARCHAR(255) NOT NULL,
-	truckModel VARCHAR(255) NOT NULL,
-	truckYear VARCHAR(255) NOT NULL,
-	sensorHoles VARCHAR(10) NOT NULL,
-	frontOrRear VARCHAR(10) NOT NULL,	
-	fogLamps VARCHAR(10) NOT NULL,
-	exhaust VARCHAR(10) NOT NULL,
-	UPC VARCHAR(255),
-	name varchar(255) NOT NULL,
-    condition VARCHAR(10) NOT NULL,
-    count INT,
-	price MONEY, 
-	PRIMARY KEY (partNumber,truckModel,condition)
+CREATE TABLE cardboardBoxes(
+	boxID INT IDENTITY,
+	boxLength VARCHAR(255) NOT NULL,
+	boxWidth VARCHAR(255) NOT NULL,
+	boxHeight VARCHAR(255) NOT NULL,
+	count INT,
+	PRIMARY KEY (boxID)
 );
-
--- "bumperShellz" created by Mitchell Evans
-CREATE TABLE bumperShellzLeft1(
-	partNumber VARCHAR(255) NOT NULL,
-	truckModel VARCHAR(255) NOT NULL,
-	truckYear VARCHAR(255) NOT NULL,
-	sensorHoles VARCHAR(10) NOT NULL,
-	frontOrRear VARCHAR(10) NOT NULL,	
-	fogLamps VARCHAR(10) NOT NULL,
-	exhaust VARCHAR(10) NOT NULL,
-	UPC VARCHAR(255),
-	name varchar(255) NOT NULL,
-    condition VARCHAR(10) NOT NULL,
-    count INT,
-	price MONEY, 
-	PRIMARY KEY (partNumber,truckModel,condition)
-);
-
--- B-Stock BumperShellz End
