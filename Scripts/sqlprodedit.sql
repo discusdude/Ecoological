bece83251171795f8ed35344378902609cfef8da
mevans27 <mitchell.evans27@gmail.com>
2017-03-08 16:10:36 -0700

    Changed to edit the sales and employee tables

--- Scripts/sqlprodedit.sql
+++ Scripts/sqlprodedit.sql
@@ -1,38 +1,23 @@
---Iventory------------------------------------------------------------------------------------	
-IF OBJECT_ID('bumperShellzRightLeft1','U') IS NOT NULL
-	DROP TABLE bumperShellzRightLeft1;
- -- B-Stock BumperShellz
- CREATE TABLE bumperShellzRight1(
+IF OBJECT_ID('sales','U') IS NOT NULL
+    DROP TABLE sales;
+CREATE TABLE sales(
 	partNumber VARCHAR(255) NOT NULL,
-	truckModel VARCHAR(255) NOT NULL,
-	truckYear VARCHAR(255) NOT NULL,
-	sensorHoles VARCHAR(10) NOT NULL,
-	frontOrRear VARCHAR(10) NOT NULL,	
-	fogLamps VARCHAR(10) NOT NULL,
-	exhaust VARCHAR(10) NOT NULL,
-	UPC VARCHAR(255),
-	name varchar(255) NOT NULL,
-    condition VARCHAR(10) NOT NULL,
-    count INT,
-	price MONEY, 
-	PRIMARY KEY (partNumber,truckModel,condition)
-);
+    date DATETIME NOT NULL,
+    empID VARCHAR(255) NOT NULL,
+    count INT NOT NULL,
+    PRIMARY KEY (date, partNumber)
+)
 
--- "bumperShellz" created by Mitchell Evans
-CREATE TABLE bumperShellzLeft1(
-	partNumber VARCHAR(255) NOT NULL,
-	truckModel VARCHAR(255) NOT NULL,
-	truckYear VARCHAR(255) NOT NULL,
-	sensorHoles VARCHAR(10) NOT NULL,
-	frontOrRear VARCHAR(10) NOT NULL,	
-	fogLamps VARCHAR(10) NOT NULL,
-	exhaust VARCHAR(10) NOT NULL,
-	UPC VARCHAR(255),
-	name varchar(255) NOT NULL,
-    condition VARCHAR(10) NOT NULL,
-    count INT,
-	price MONEY, 
-	PRIMARY KEY (partNumber,truckModel,condition)
-);
+IF OBJECT_ID('employees','U') IS NOT NULL
+    DROP TABLE employees;
+CREATE TABLE employees(
+    empID VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    deptID INT,
+    DoB DATE NOT NULL,
+    CONSTRAINT FK_employees_departments FOREIGN KEY (deptID) REFERENCES departments(deptID) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (empId)
+)
 
 -- B-Stock BumperShellz End
