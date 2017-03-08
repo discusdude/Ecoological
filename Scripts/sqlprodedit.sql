53c4b04d2b7da1779c6656cc0930d588a99d81b1
mevans27 <mitchell.evans27@gmail.com>
2017-03-08 16:14:40 -0700

    Using to change Sales and Employee tables

--- Scripts/sqlprodedit.sql
+++ Scripts/sqlprodedit.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 IF OBJECT_ID('sales','U') IS NOT NULL
     DROP TABLE sales;
 CREATE TABLE sales(
@@ -20,19 +20,3 @@ CREATE TABLE employees(
     CONSTRAINT FK_employees_departments FOREIGN KEY (deptID) REFERENCES departments(deptID) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (empId)
 )
-
--- B-Stock BumperShellz End
-=======
---Iventory------------------------------------------------------------------------------------	
-IF OBJECT_ID('cardboardBoxes','U') IS NOT NULL
-	DROP TABLE cardboardBoxes;
- -- B-Stock BumperShellz
-CREATE TABLE cardboardBoxes(
-	boxID INT IDENTITY,
-	boxLength VARCHAR(255) NOT NULL,
-	boxWidth VARCHAR(255) NOT NULL,
-	boxHeight VARCHAR(255) NOT NULL,
-	count INT,
-	PRIMARY KEY (boxID)
-);
->>>>>>> 703945d8baa3d717ad672997d71d598840970618
